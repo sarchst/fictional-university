72976d3749131b9d0dfc7462a2d4adf760cd63f2
sarch <sarchenstarke@gmail.com>
2019-04-14 20:36:00 -0700

    added campus post type, search class, implemented search bar, now responds to text input, no search is being done yet

--- app/public/wp-content/themes/fictional-university-theme/archive-campus.php
+++ app/public/wp-content/themes/fictional-university-theme/archive-campus.php
@@ -1,31 +1,32 @@
-<!-- this page is used to power archive pages-->
 <?php
+
 get_header();
 pageBanner(array(
-  'title'=>'Our Campuses',
-  'subtitle'=>'We have several convenient campuses'
+  'title' => 'Our Campuses',
+  'subtitle' => 'We have several conveniently located campuses.'
 ));
-?>
-
+ ?>
 
 <div class="container container--narrow page-section">
-  <div class="acf-map">
-  <?php
-  while(have_posts()) {
-    the_post();
-    $mapLocation = get_field('map_location');?>
-    <div class="marker" data-lat="<?php echo $mapLocation['lat']?>" data-lng="<?php echo $mapLocation['lng']?>" ?>>
 
-    </div>
-  <?php }
-  echo paginate_links();
-  ?>
-</div>
-
-</div>
-
-
+<div class="acf-map">
 
 <?php
-get_footer();
+  while(have_posts()) {
+    the_post();
+    $mapLocation = get_field('map_location');
+   ?>
+    <div class="marker" data-lat="<?php echo $mapLocation['lat'] ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+      <h3><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h3>
+      <?php echo $mapLocation['address'] ?>
+    </div>
+  <?php } ?>
+</div>
+
+
+
+</div>
+
+<?php get_footer();
+
 ?>
